ef796d70486103fe646997ce679ed75e0084c35b
Mevlüt Çelik <23mevlutcelik@gmail.com>
2022-05-26 15:16:04 +0300

    Create app.js
    Calculated the screen height of the browser.

---
+++ js/app.js
@@ -0,0 +1,6 @@
+function appHeight() {
+    const e = document.documentElement;
+    e.style.setProperty("--vh", .01 * window.innerHeight + "px")
+}
+
+window.addEventListener("resize", appHeight), appHeight();
